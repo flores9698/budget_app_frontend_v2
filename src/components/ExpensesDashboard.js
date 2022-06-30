7eb30f4b72c3b72fd4d2589f016c9ce9dab675c8
saulflores3126 <saulflores3126@gmail.com>
2022-06-30 09:22:13 -0600

    Updated Backend Ip Address

--- src/components/ExpensesDashboard.js
+++ src/components/ExpensesDashboard.js
@@ -1,167 +1,156 @@
-import React from 'react';
+import React from "react";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
-import {Cookies} from "react-cookie";
-import {useEffect} from "react";
+import { Cookies } from "react-cookie";
+import { useEffect } from "react";
 import axios from "axios";
-import {Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import AddCategoryDialog from "./dialogs/AddCategoryDialog";
 import AddExpenseDialog from "./dialogs/AddExpenseDialog";
 // import {Table} from 'react-bootstrap';
 const cookies = new Cookies();
-const cookiesOptions = {expires: new Date(Date.now() + 3600 * 1000)}
+const cookiesOptions = { expires: new Date(Date.now() + 3600 * 1000) };
 const userId = cookies.get("userid");
 const token = cookies.get("token");
-const baseUrl = 'http://192.168.0.14:8500';
-
+const baseUrl = "http://34.67.5.157:8500";
 
 export default function ExpensesDashboard() {
-    const [categories, setCategories] = React.useState([]);
-    const [expenses, setExpenses] = React.useState([]);
-    const [order, setOrder] = React.useState('ASC');
+  const [categories, setCategories] = React.useState([]);
+  const [expenses, setExpenses] = React.useState([]);
+  const [order, setOrder] = React.useState("ASC");
 
-    const sorting = (col) => {
-        if (order === 'ASC'){
-            const sorted = [...expenses].sort((a,b) =>
-            a[col] > b[col] ? 1 : -1
-        );
-        setExpenses(sorted);
-        setOrder('DESC');
-        }
-        if (order === 'DESC'){
-            const sorted = [...expenses].sort((a,b) =>
-            a[col] < b[col] ? -1 : 1
-        );
-        setExpenses(sorted);
-        setOrder('ASC');
-        }
+  const sorting = (col) => {
+    if (order === "ASC") {
+      const sorted = [...expenses].sort((a, b) => (a[col] > b[col] ? 1 : -1));
+      setExpenses(sorted);
+      setOrder("DESC");
     }
-
-    const getCategories = async () => {
-        const response = await fetch(
-            `${baseUrl}/categories/${userId}`,
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`
-                },
-            }
-        );
-        const data = await response.json();
-        console.log(data.body);
-        setCategories(data.body.categories.categories);
-
+    if (order === "DESC") {
+      const sorted = [...expenses].sort((a, b) => (a[col] < b[col] ? -1 : 1));
+      setExpenses(sorted);
+      setOrder("ASC");
     }
+  };
 
-    const getExpenses = async () => {
-        const response = await axios.get(
-            `${baseUrl}/expenses/${userId}`,
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`
-                }
+  const getCategories = async () => {
+    const response = await fetch(`${baseUrl}/categories/${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+    console.log(data.body);
+    setCategories(data.body.categories.categories);
+  };
 
-            }
+  const getExpenses = async () => {
+    const response = await axios.get(`${baseUrl}/expenses/${userId}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    setExpenses((await response).data.body.expenses);
+  };
+  useEffect(() => {
+    getExpenses();
+    console.log("Expenses", expenses);
+  }, []);
+  //sort expenses by column
+  const sortExpenses = (column) => {
+    const sortedExpenses = expenses.sort((a, b) => {
+      if (a[column] < b[column]) {
+        return -1;
+      }
+      if (a[column] > b[column]) {
+        return 1;
+      }
+      return 0;
+    });
+    setExpenses(sortedExpenses);
+  };
 
-        );
-        setExpenses((await response).data.body.expenses);
-    }
-    useEffect(() => {
-            getExpenses();
-            console.log("Expenses",expenses);
+  return (
+    <div className={"container bg-gradient my-5"}>
+      <div className={"container-fluid"}>
+        <Typography variant={"h4"}>Expenses</Typography>
+        <Divider />
 
-        }
-        , []);
-    //sort expenses by column
-    const sortExpenses = (column) => {
-        const sortedExpenses = expenses.sort((a, b) => {
-            if (a[column] < b[column]) {
-                return -1;
-            }
-            if (a[column] > b[column]) {
-                return 1;
-            }
-            return 0;
-        });
-        setExpenses(sortedExpenses);
-    }
+        <Typography variant={"h3"} mt={10}>
+          Last Expenses
+        </Typography>
 
-    return (
-        <div className={'container bg-gradient my-5'}>
-            <div className={"container-fluid"}>
-                <Typography variant={"h4"}>
-                    Expenses
-                </Typography>
-                <Divider/>
-
-                <Typography variant={"h3"} mt={10}>
-                    Last Expenses
-                </Typography>
-
-                    {/*<Table>*/}
-                    {/*    <TableHead>*/}
-                    {/*        <TableRow>*/}
-                    {/*            <TableCell>*/}
-                    {/*                Description*/}
-                    {/*            </TableCell>*/}
-                    {/*            <TableCell>*/}
-                    {/*                Date*/}
-                    {/*            </TableCell>*/}
-                    {/*            <TableCell>*/}
-                    {/*                Amount*/}
-                    {/*            </TableCell>*/}
-                    {/*            <TableCell>*/}
-                    {/*                Category*/}
-                    {/*            </TableCell>*/}
-                    {/*        </TableRow>*/}
-                    {/*    </TableHead>*/}
-                    {/*    <TableBody>*/}
-                    {/*        {expenses.map((expense) => (*/}
-                    {/*            <TableRow key={expense.id+expense.expense_name}>*/}
-                    {/*                <TableCell>*/}
-                    {/*                    {expense.expense_name}*/}
-                    {/*                </TableCell>*/}
-                    {/*                <TableCell>*/}
-                    {/*                    {expense.date_added.split("T")[0]}*/}
-                    {/*                </TableCell>*/}
-                    {/*                <TableCell>*/}
-                    {/*                    {expense.amount}*/}
-                    {/*                </TableCell>*/}
-                    {/*                <TableCell>*/}
-                    {/*                    {expense.category_name}*/}
-                    {/*                </TableCell>*/}
-                    {/*            </TableRow>*/}
-                    {/*        ))}*/}
-                    {/*    </TableBody>*/}
-                    {/*</Table>*/}
-                <Table className={"table table-striped "} >
-                    <TableHead>
-                        <TableRow>
-                            <TableCell hover onClick={()=>sorting('expense_name')}>Description</TableCell>
-                            <TableCell onClick={()=>sorting('date_added')}>Date</TableCell>
-                            <TableCell onClick={()=>sorting('amount')}>Amount</TableCell>
-                            <TableCell onClick={()=>sorting('category_name')}>Category</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {expenses.map((expense) => (
-                            <TableRow key={expense.id+expense.expense_name}>
-                                <TableCell>{expense.expense_name}</TableCell>
-                                <TableCell>{expense.date_added.split("T")[0]}</TableCell>
-                                <TableCell>{expense.amount}</TableCell>
-                                <TableCell>{expense.category_name}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-
-                </Table>
-                <AddCategoryDialog/>
-                <AddExpenseDialog/>
-
-
-            </div>
-        </div>
-    );
+        {/*<Table>*/}
+        {/*    <TableHead>*/}
+        {/*        <TableRow>*/}
+        {/*            <TableCell>*/}
+        {/*                Description*/}
+        {/*            </TableCell>*/}
+        {/*            <TableCell>*/}
+        {/*                Date*/}
+        {/*            </TableCell>*/}
+        {/*            <TableCell>*/}
+        {/*                Amount*/}
+        {/*            </TableCell>*/}
+        {/*            <TableCell>*/}
+        {/*                Category*/}
+        {/*            </TableCell>*/}
+        {/*        </TableRow>*/}
+        {/*    </TableHead>*/}
+        {/*    <TableBody>*/}
+        {/*        {expenses.map((expense) => (*/}
+        {/*            <TableRow key={expense.id+expense.expense_name}>*/}
+        {/*                <TableCell>*/}
+        {/*                    {expense.expense_name}*/}
+        {/*                </TableCell>*/}
+        {/*                <TableCell>*/}
+        {/*                    {expense.date_added.split("T")[0]}*/}
+        {/*                </TableCell>*/}
+        {/*                <TableCell>*/}
+        {/*                    {expense.amount}*/}
+        {/*                </TableCell>*/}
+        {/*                <TableCell>*/}
+        {/*                    {expense.category_name}*/}
+        {/*                </TableCell>*/}
+        {/*            </TableRow>*/}
+        {/*        ))}*/}
+        {/*    </TableBody>*/}
+        {/*</Table>*/}
+        <Table className={"table table-striped "}>
+          <TableHead>
+            <TableRow>
+              <TableCell hover onClick={() => sorting("expense_name")}>
+                Description
+              </TableCell>
+              <TableCell onClick={() => sorting("date_added")}>Date</TableCell>
+              <TableCell onClick={() => sorting("amount")}>Amount</TableCell>
+              <TableCell onClick={() => sorting("category_name")}>
+                Category
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {expenses.map((expense) => (
+              <TableRow key={expense.id + expense.expense_name}>
+                <TableCell>{expense.expense_name}</TableCell>
+                <TableCell>{expense.date_added.split("T")[0]}</TableCell>
+                <TableCell>{expense.amount}</TableCell>
+                <TableCell>{expense.category_name}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <AddCategoryDialog />
+        <AddExpenseDialog />
+      </div>
+    </div>
+  );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,145 +1,141 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import AccountBalanceSharpIcon from '@mui/icons-material/AccountBalanceSharp';
-import {Cookies} from "react-cookie";
-import {Link, useNavigate} from "react-router-dom";
-import {useEffect} from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import AccountBalanceSharpIcon from "@mui/icons-material/AccountBalanceSharp";
+import { Cookies } from "react-cookie";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import axios from "axios";
-import {TableBody, TableCell, TableHead, TableRow, Table, TableContainer, Button} from "@mui/material";
-import {Routes, Route, Outlet} from "react-router-dom";
+import {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Table,
+  TableContainer,
+  Button,
+} from "@mui/material";
+import { Routes, Route, Outlet } from "react-router-dom";
 import AddAccountDialog from "./dialogs/AddAccountDialog";
 import ExpensesDashboard from "./ExpensesDashboard";
 import AccountTable from "./AccountTable";
 
 const drawerWidth = 240;
 const cookies = new Cookies();
-const cookiesOptions = {expires: new Date(Date.now() + 3600 * 1000)}
-const baseUrl = 'http://192.168.0.14:8500';
+const cookiesOptions = { expires: new Date(Date.now() + 3600 * 1000) };
+const baseUrl = "http://34.67.5.157:8500";
 
 export default function Sidebar() {
-    const [userInfo, setUserInfo] = React.useState({});
-    const [bankAccounts, setBankAccounts] = React.useState([]);
+  const [userInfo, setUserInfo] = React.useState({});
+  const [bankAccounts, setBankAccounts] = React.useState([]);
 
-    const authToken = cookies.get("token");
-    const userId = cookies.get("userid");
+  const authToken = cookies.get("token");
+  const userId = cookies.get("userid");
 
-    const getBankAccounts = async () => {
-        const response = await fetch(
-            `${baseUrl}/bank_accounts/${userId}`,
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${authToken}`
-                },
-            }
-        );
-        const data = await response.json();
-        setBankAccounts(data.body.user)
-        // cookies.set("bankAccounts", data.body.user, cookiesOptions);
-    };
-    React.useEffect(() => {
-        getBankAccounts();
-        console.log(bankAccounts);
+  const getBankAccounts = async () => {
+    const response = await fetch(`${baseUrl}/bank_accounts/${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
+    const data = await response.json();
+    setBankAccounts(data.body.user);
+    // cookies.set("bankAccounts", data.body.user, cookiesOptions);
+  };
+  React.useEffect(() => {
+    getBankAccounts();
+    console.log(bankAccounts);
+  }, []);
 
-    }, []);
-
-
-    const navigate = useNavigate();
-    return (
-        <Box sx={{display: 'flex'}}>
-            <CssBaseline/>
-            <AppBar
-                // position="fixed"
-                sx={{width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px`,}}
-            >
-                <Toolbar>
-
-                    <ListItemButton onClick={() => {
-                        console.log("logout");
-                        cookies.remove("token");
-                        cookies.remove("userid");
-                        navigate("/login");
-
-
-                    }}>
-                        <ListItemText primary={"Logout"}/>
-
-                    </ListItemButton>
-                </Toolbar>
-
-            </AppBar>
-            <Drawer
+  const navigate = useNavigate();
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        // position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+        <Toolbar>
+          <ListItemButton
+            onClick={() => {
+              console.log("logout");
+              cookies.remove("token");
+              cookies.remove("userid");
+              navigate("/login");
+            }}
+          >
+            <ListItemText primary={"Logout"} />
+          </ListItemButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          color: "#1b77d2",
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar
+          sx={{
+            backgroundColor: "#1b77d2",
+            color: "#fff",
+          }}
+        >
+          <Typography variant="h6">My Budget App</Typography>
+        </Toolbar>
+        <Divider />
+        <List mt={3}>
+          {["Expenses", "Accounts"].map((element) => (
+            <ListItem key={element} disablePadding>
+              <AccountBalanceSharpIcon />
+              <ListItemButton
                 sx={{
-                    width: drawerWidth,
-                    color: '#1b77d2',
-                    flexShrink: 0,
-                    '& .MuiDrawer-paper': {
-                        width: drawerWidth,
-                        boxSizing: 'border-box',
-                    },
+                  textDecoration: "none",
                 }}
-                variant="permanent"
-                anchor="left"
-            >
-                <Toolbar sx={{
-                    backgroundColor: '#1b77d2',
-                    color: '#fff',
-                }}>
-                    <Typography variant="h6">
-                        My Budget App
-                    </Typography>
-                </Toolbar>
-                <Divider/>
-                <List mt={3}>
-                    {
-                        ['Expenses', 'Accounts'].map((element) => (
-                            <ListItem key={element} disablePadding>
-                                <AccountBalanceSharpIcon/>
-                                <ListItemButton sx={{
-                                    textDecoration: 'none',
-                                }} >
-                                    <Link to={`${element.toLowerCase()}`} >
-                                        <ListItemText primary={element}/>
-                                    </Link>
-                                </ListItemButton>
-                            </ListItem>
+              >
+                <Link to={`${element.toLowerCase()}`}>
+                  <ListItemText primary={element} />
+                </Link>
+              </ListItemButton>
+            </ListItem>
+          ))}
+          <Divider />
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: "background.default ", p: 3 }}
+      >
+        {/*<Box mt={8}>*/}
+        {/*    <AccountTable/>*/}
+        {/*</Box>*/}
 
-                        ))}
-                    <Divider/>
-                </List>
+        {/*<Box mt={10}>*/}
+        {/*    <ExpensesDashboard/>*/}
 
-            </Drawer>
-            <Box
-                component="main"
-                sx={{flexGrow: 1, bgcolor: 'background.default ', p: 3}}
-            >
-
-
-                {/*<Box mt={8}>*/}
-                {/*    <AccountTable/>*/}
-                {/*</Box>*/}
-
-                {/*<Box mt={10}>*/}
-                {/*    <ExpensesDashboard/>*/}
-
-                {/*</Box>*/}
-                <Outlet/>
-
-            </Box>
-        </Box>
-    );
+        {/*</Box>*/}
+        <Outlet />
+      </Box>
+    </Box>
+  );
 }
